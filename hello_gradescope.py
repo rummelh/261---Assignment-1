0a313aae919e539443c6040ab7bc2839cebc67ed
rummelh <rummelh@oregonstate.edu>
2023-01-17 11:11:48 -0600

    xx

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name: Hannah Rummel
+# OSU Email: rummelh@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0
+# Due Date: 1/18/2023
+# Description: Hello Gradescope test
+
+
+def gradescope_test():
+    return 42
